79f36f53aba9fefd2bb290cfe1eb00d364f489fd
blessedsoy <unmigong@gmail.com>
2017-02-07 00:16:58 -0500

    Done.

---
+++ components/Bomb.js
@@ -0,0 +1,20 @@
+const React = require('react');
+
+class Bomb extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      secondsLeft: props.initialCount
+    };
+  }
+  
+
+  render() {
+    return(
+      <div>{this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`}
+      </div>
+    );
+  }
+}
+
+module.exports = Bomb;
---
+++ components/ImageSlider.js
@@ -0,0 +1,37 @@
+const React = require('react');
+
+class ImageSlider extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    };
+  }
+  render() {
+    return (
+      <div>I am on slide {this.state.currentSlideIndex}</div>
+    );
+  }
+}
+
+module.exports = ImageSlider;
+
+// const React = require('react');
+
+// class ImageSlider extends React.Component {
+//   constructor(props) {
+//     super();
+
+//     this.state = {
+//       currentSlideIndex: 0,
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div>I am on slide {this.state.currentSlideIndex}</div>
+//     );
+//   }
+// }
+
+// module.exports = ImageSlider;
